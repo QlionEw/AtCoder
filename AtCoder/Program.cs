6b06b17a51177d91b7e134018a3e85dc5a4d1986
Qlion <lovage.ew@gmail.com>
2022-03-26 20:54:43 +0900

    TreeにVisitTime追加(暫定)

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -1905,18 +1905,19 @@ namespace AtCoder
 
     public class TreeStructure
     {
-        public class TreePath
+        public class TreeVertex
         {
             public List<int> Ways { get; } = new List<int>();
             public List<long> Costs { get; } = new List<long>();
+            public Set<int> VisitTime { get; } = new Set<int>();
 
             public IEnumerable<(int Way, long Cost)> GetWayData() => Ways.Select((t, i) => (t, Costs[i]));
         }
 
-        public TreePath[] Vertexes { get; }
+        public TreeVertex[] Vertexes { get; }
         public TreeStructure(int size)
         {
-            Vertexes = Enumerable.Range(0, size + 1).Select(_ => new TreePath()).ToArray();
+            Vertexes = Enumerable.Range(0, size + 1).Select(_ => new TreeVertex()).ToArray();
         }
 
         public void Connect(int index, int index2, long cost = 1)
@@ -1938,8 +1939,8 @@ namespace AtCoder
             if (isTouring)
             {
                 int time = 0;
-                tourTime = new List<(int, int)>();
-                firstTime = new int[Vertexes.Length];
+                TourTime = new List<(int, int)>();
+                FirstTime = new int[Vertexes.Length];
                 CheckCostAndTour(origin, 0, 0, ref time);
             }
             else
@@ -1949,16 +1950,17 @@ namespace AtCoder
             return costArray;
         }
 
-        private List<(int, int)> tourTime;
-        private int[] firstTime;
+        public List<(int, int)> TourTime { get; set; }
+        public int[] FirstTime { get; set; }
         private void CheckCostAndTour(int current, long cost, int from, ref int time, int depth = 0)
         {
             costArray[current] = cost;
-            if (firstTime[current] == 0)
+            if (FirstTime[current] == 0)
             {
-                firstTime[current] = time++;
+                FirstTime[current] = time++;
             }
-            tourTime.Add((current, depth));
+            TourTime.Add((current, depth));
+            Vertexes[current].VisitTime.Add(FirstTime[current]);
             foreach ((int way, long l) in Vertexes[current].GetWayData())
             {
                 if (way == from)
@@ -1966,7 +1968,8 @@ namespace AtCoder
                     continue;
                 }
                 CheckCostAndTour(way, cost + l, current, ref time, depth + 1);
-                tourTime.Add((current, depth));
+                TourTime.Add((current, depth));
+                Vertexes[current].VisitTime.Add(time);
                 time++;
             }
         }
@@ -1992,8 +1995,8 @@ namespace AtCoder
             {
                 InitLca();
             }
-            var min = Math.Min(firstTime[index1], firstTime[index2]);
-            var max = Math.Max(firstTime[index1], firstTime[index2]);
+            var min = Math.Min(FirstTime[index1], FirstTime[index2]);
+            var max = Math.Max(FirstTime[index1], FirstTime[index2]);
             var item = seg.Query(min, max);
             return item.Item1;
         }
@@ -2001,8 +2004,8 @@ namespace AtCoder
         private void InitLca()
         {
             seg = new SegmentTreeExtend<(int,int)>();
-            seg.Init(tourTime.Count, (Common.InfinityInt,Common.InfinityInt), (a,b) => a.Item2 < b.Item2 ? a : b);
-            seg.Build(i => tourTime[i]);
+            seg.Init(TourTime.Count, (Common.InfinityInt,Common.InfinityInt), (a,b) => a.Item2 < b.Item2 ? a : b);
+            seg.Build(i => TourTime[i]);
         }
 
         public int GetDistance(int index1, int index2)
@@ -2012,8 +2015,8 @@ namespace AtCoder
                 InitLca();
             }
             var target = GetLowestCommonAncestor(index1, index2);
-            return tourTime[firstTime[index1]].Item2 + tourTime[firstTime[index2]].Item2 -
-                   2 * tourTime[firstTime[target]].Item2;
+            return TourTime[FirstTime[index1]].Item2 + TourTime[FirstTime[index2]].Item2 -
+                   2 * TourTime[FirstTime[target]].Item2;
         }
 
         public long GetCost(int index1, int index2)
