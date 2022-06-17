9a920d31dca304b53f5ea1b9ef92dd7985603308
Qlion <lovage.ew@gmail.com>
2022-06-18 02:23:19 +0900

    提出をそのまま載せてしまっていたので削除

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -20,40 +20,6 @@ namespace AtCoder
             SourceExpander.Expander.Expand();
             checked
             {
-                int n = Si();
-                int[] a = Sai(n);
-                int q = Si();
-                int[][] lr = Sqi(q, 2);
- 
-                var mo = new Mo(n, q);
-                foreach (var i in lr)
-                {
-                    mo.Insert(i[0] - 1, i[1] - 1);
-                }
- 
-                int[] ans = new int[q]; 
-                int total = 0;
-                var d = new int[n + 1];
-                mo.Run(i =>
-                {
-                    if (d[a[i]] % 2 == 1)
-                    {
-                        total++;
-                    }
-                    d[a[i]]++;
-                }, i =>
-                {
-                    if (d[a[i]] % 2 == 0)
-                    {
-                        total--;
-                    }
-                    d[a[i]]--;
-                }, i =>
-                {
-                    ans[i] = total;
-                });
- 
-                Answerer.OutAllLine(ans);
             }
         }
     }
