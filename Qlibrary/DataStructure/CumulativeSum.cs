db83f5e6ee79b32b9ae330f8dc618c010580fa4d
Qlion <lovage.ew@gmail.com>
2022-11-04 16:07:00 +0900

    一応一次元と二次元累積和

---
+++ Qlibrary/DataStructure/CumulativeSum.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Qlibrary
+{
+    public class CumulativeSum : IEnumerable<long>
+    {
+        private bool isCalculated;
+        private long[] box;
+        
+        public CumulativeSum(int size)
+        {
+            box = new long[size];
+        }
+
+        [MethodImpl(256)]
+        public void Add(int index, long value)
+        {
+            box[index] += value;
+        }
+        
+        [MethodImpl(256)]
+        public void AddRange(int start, int end, long value)
+        {
+            box[start] += value;
+            if (end + 1 < box.Length)
+            {
+                box[end + 1] -= value;
+            }
+        }
+
+        public void Calculate()
+        {
+            for (int i = 0; i < box.Length - 1; i++)
+            {
+                box[i + 1] += box[i];
+            }
+            isCalculated = true;
+        }
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            if (!isCalculated)
+            {
+                Calculate();
+            }
+            return box.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            if (!isCalculated)
+            {
+                Calculate();
+            }
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Qlibrary/DataStructure/CumulativeSum2D.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Qlibrary
+{
+    public class CumulativeSum2D : IEnumerable<IEnumerable<long>>
+    {
+        private bool isCalculated;
+        private long[][] box;
+        
+        public CumulativeSum2D(int height, int width)
+        {
+            box = Enumerable.Repeat(0, height).Select(_ => new long[width]).ToArray();
+        }
+
+        [MethodImpl(256)]
+        public void Add(int height, int width, long value)
+        {
+            box[height][width] += value;
+        }
+        
+        [MethodImpl(256)]
+        public void AddSquare((int h,int w) start, (int h,int w) end, long value)
+        {
+            box[start.h][start.w] += value;
+            if (end.h + 1 < box.Length)
+            {
+                box[end.h + 1][start.w] -= value;
+            }
+            if (end.w + 1 < box[0].Length)
+            {
+                box[start.h][end.w + 1] -= value;
+            }
+            if (end.h + 1 < box.Length && end.w + 1 < box[0].Length)
+            {
+                box[end.h + 1][end.w + 1] += value;
+            }
+        }
+
+        public void Calculate()
+        {
+            foreach (var row in box)
+            {
+                for (int j = 0; j < box[0].Length - 1; j++)
+                {
+                    row[j + 1] += row[j];
+                }
+            }
+                    
+            for (int i = 0; i < box[0].Length; i++)
+            {
+                for (int j = 0; j < box.Length - 1; j++)
+                {
+                    box[j + 1][i] += box[j][i];
+                }
+            }
+            isCalculated = true;
+        }
+
+        public IEnumerator<IEnumerable<long>> GetEnumerator()
+        {
+            if (!isCalculated)
+            {
+                Calculate();
+            }
+            return box.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            if (!isCalculated)
+            {
+                Calculate();
+            }
+            return GetEnumerator();
+        }
+    }
+}
