1962e269b46d47ee9377703e26de8d5fbc15dec1
Qlion <lovage.ew@gmail.com>
2023-04-21 20:41:37 +0900

    双方向Dictionary追加

---
+++ Qlibrary/Container/TwoWayDictionary.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace Qlibrary
+{
+    public class TwoWayDictionary<T1, T2>
+    {
+        public Dictionary<T1, T2> Fore { get; } = new Dictionary<T1, T2>();
+        public Dictionary<T2, T1> Back { get; } = new Dictionary<T2, T1>();
+
+        public void Add(T1 key, T2 value)
+        {
+            Fore.Add(key, value);
+            Back.Add(value, key);
+        }
+
+        public void Remove(T1 key)
+        {
+            var value = Fore[key];
+            Fore.Remove(key);
+            Back.Remove(value);
+        }
+
+        public void Clear()
+        {
+            Fore.Clear();
+            Back.Clear();
+        }
+    }
+}
