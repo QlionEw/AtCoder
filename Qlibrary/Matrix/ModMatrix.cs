835da8454b10618950bd608519d4506e80611cc5
Qlion <lovage.ew@gmail.com>
2023-06-13 02:27:48 +0900

    ModMatrixバグ修正・速度改善

--- Qlibrary/Matrix/ModMatrix.cs
+++ Qlibrary/Matrix/ModMatrix.cs
@@ -1,21 +1,26 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Qlibrary
 {
-    public class ModMatrix
+    public readonly struct ModMatrix 
     {
-        private readonly ModInt[][] box;
+        private readonly long[][] box;
         public int Height { get; }
         public int Width { get; }
-        public ModInt[] this[int k] { get => box[k]; set => box[k] = value; }
+        public long[] this[int k]
+        {
+            [MethodImpl(256)]get => box[k];
+            [MethodImpl(256)]set => box[k] = value;
+        }
 
         public ModMatrix(int h, int w)
         {
             Height = h;
             Width = w;
-            box = Enumerable.Range(0, h).Select(x => new ModInt[w]).ToArray();
+            box = Array.ConvertAll(new bool[h], _ =>  new long[w]);
         }
 
         public ModMatrix I()
@@ -26,29 +31,42 @@ namespace Qlibrary
             return mat;
         }
 
+        [MethodImpl(256)]
         public static ModMatrix operator +(ModMatrix a, ModMatrix b)
         {
             for (int i = 0; i < a.Height; i++)
             for (int j = 0; j < a.Width; j++)
+            {
                 a[i][j] += b[i][j];
+                a[i][j] %= ModInt.ModValue;
+            }
             return a;
         }
 
+        [MethodImpl(256)]
         public static ModMatrix operator -(ModMatrix a, ModMatrix b)
         {
             for (int i = 0; i < a.Height; i++)
             for (int j = 0; j < a.Width; j++)
+            {
                 a[i][j] -= b[i][j];
+                if(a[i][j] < 0) a[i][j] += ModInt.ModValue;
+            }
             return a;
         }
 
+        [MethodImpl(256)]
         public static ModMatrix operator *(ModMatrix a, ModMatrix b)
         {
+            var n = a.Height;
             var c = new ModMatrix(a.Height, a.Width);
-            for (int i = 0; i < a.Height; i++)
-            for (int k = 0; k < a.Height; k++)
-            for (int j = 0; j < a.Height; j++)
-                c[i][j] = a[i][k] * b[k][j];
+            for (int i = 0; i < n; i++)
+            for (int k = 0; k < n; k++)
+            for (int j = 0; j < n; j++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+                c[i][j] %= ModInt.ModValue;
+            }
             return c;
         }
 
@@ -61,7 +79,6 @@ namespace Qlibrary
                 a *= a;
                 k >>= 1;
             }
-
             return b;
         }
     }
