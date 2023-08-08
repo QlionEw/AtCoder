6d814bf227785355657fc8025023c4bc519b442c
Qlion <lovage.ew@gmail.com>
2023-08-09 01:08:54 +0900

    プロジェクトファイルと提出ファイルをジャッジにある程度合わせる

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -3,14 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Text;
 using static System.Math;
 using Qlibrary;
 using static Qlibrary.Common;
 using static Qlibrary.MathPlus;
 
-namespace AtCoder
+namespace Qompetitive
 {
     internal static class Program
     {
