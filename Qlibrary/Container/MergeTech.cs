7adf8d2c52f4f4f077b70334ea8df96adf193dd0
Qlion <lovage.ew@gmail.com>
2023-10-28 17:46:08 +0900

    Genericクラスに変更

--- Qlibrary/Container/MergeTech.cs
+++ Qlibrary/Container/MergeTech.cs
@@ -5,18 +5,28 @@ using System.Linq;
 
 namespace Qlibrary
 {
-    public class MergeTech
+    public class MergeTechOperator<T>
+    {
+        public Func<int, T> Init { get; set; }
+        public Func<T, int> Count { get; set; }
+        public Action<(T From, T To)> Merge { get; set; }
+    }
+    
+    public class MergeTech<T>
     {
         private readonly int n;
-        private readonly List<List<int>> arrays = new();
+        private readonly MergeTechOperator<T> op;
+        private readonly T[] arrays;
         private readonly UnionFindTree uft;
 
-        public MergeTech(int n)
+        public MergeTech(int n, MergeTechOperator<T> op)
         {
             this.n = n;
-            for (int i = 0; i <= n; i++)
+            this.op = op;
+            arrays = new T[n + 1];
+            for (int i = 1; i <= n; i++)
             {
-                arrays.Add(new List<int> { i });
+                arrays[i] = op.Init(i);
             }
             uft = new UnionFindTree(n + 1);
         }
@@ -27,12 +37,12 @@ namespace Qlibrary
             int b = uft.Find(i1);
             uft.Unite(i0, i1);
             int x = uft.Find(i1);
-            var inQue = arrays[a].Count > arrays[b].Count ? arrays[a] : arrays[b];
-            var outQue = arrays[a].Count > arrays[b].Count ? arrays[b] : arrays[a];
-            inQue.AddRange(outQue);
+            var inQue = op.Count(arrays[a]) > op.Count(arrays[b]) ? arrays[a] : arrays[b];
+            var outQue = op.Count(arrays[a]) > op.Count(arrays[b]) ? arrays[b] : arrays[a];
+            op.Merge((outQue, inQue));
             arrays[x] = inQue;
         }
     
-        public List<int> GetMerged(int index = 1) => arrays[uft.Find(index)];
+        public T GetMerged(int index = 1) => arrays[uft.Find(index)];
     }
 }
