8e08cfa9d380bddb6cbba0853cf6575332c97eab
Qlion <lovage.ew@gmail.com>
2023-12-16 18:07:50 +0900

    操作中により情報を得られるようにする

--- Qlibrary/Container/MergeTech.cs
+++ Qlibrary/Container/MergeTech.cs
@@ -5,41 +5,36 @@ using System.Linq;
 
 namespace Qlibrary
 {
-    public class MergeTechOperator<T>
-    {
-        public Func<int, T> Init { get; set; }
-        public Func<T, int> Count { get; set; }
-        public Action<(T From, T To)> Merge { get; set; }
-    }
-    
     public class MergeTech<T>
     {
         private readonly int n;
-        private readonly MergeTechOperator<T> op;
+        private readonly Func<T, int> count;
         private readonly T[] arrays;
         private readonly UnionFindTree uft;
 
-        public MergeTech(int n, MergeTechOperator<T> op)
+        public MergeTech(int n, Func<int, T> init, Func<T, int> count)
         {
             this.n = n;
-            this.op = op;
+            this.count = count;
             arrays = new T[n + 1];
             for (int i = 1; i <= n; i++)
             {
-                arrays[i] = op.Init(i);
+                arrays[i] = init(i);
             }
             uft = new UnionFindTree(n + 1);
         }
 
-        public void Merge(int i0, int i1)
+        public void Merge(int i0, int i1, Action<(T From, int FromIndex, T To, int ToIndex)> merge)
         {
             int a = uft.Find(i0);
             int b = uft.Find(i1);
             uft.Unite(i0, i1);
             int x = uft.Find(i1);
-            var inQue = op.Count(arrays[a]) > op.Count(arrays[b]) ? arrays[a] : arrays[b];
-            var outQue = op.Count(arrays[a]) > op.Count(arrays[b]) ? arrays[b] : arrays[a];
-            op.Merge((outQue, inQue));
+            var inQue = count(arrays[a]) > count(arrays[b]) ? arrays[a] : arrays[b];
+            var outQue = count(arrays[a]) > count(arrays[b]) ? arrays[b] : arrays[a];
+            var inIndex = count(arrays[a]) > count(arrays[b]) ? i0 : i1;
+            var outIndex = count(arrays[a]) > count(arrays[b]) ? i1 : i0;
+            merge((outQue, outIndex, inQue, inIndex));
             arrays[x] = inQue;
         }
     
