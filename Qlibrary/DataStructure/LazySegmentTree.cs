a9df32eb96c706e69179237062979d3a7a6d76fe
Qlion <lovage.ew@gmail.com>
2024-01-03 15:58:36 +0900

    遅延セグ木のパラメータの説明やデバッグを追加

--- Qlibrary/DataStructure/LazySegmentTree.cs
+++ Qlibrary/DataStructure/LazySegmentTree.cs
@@ -1,81 +1,54 @@
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Qlibrary
 {
-    public enum LazySegmentTreeMode
-    {
-        UpdateMin,
-        UpdateMax,
-        Addition,
-        ModAddition,
-    }
-
-    public class LazySegmentTree : LazySegmentTreeExtend<long>
-    {
-        public LazySegmentTree(int count, LazySegmentTreeMode mode) : base(count, GetInvalidValue(mode))
-        {
-            SetMode(mode);
-        }
-
-        private void SetMode(LazySegmentTreeMode mode)
-        {
-            UpdateMethod = mode switch
-            {
-                LazySegmentTreeMode.UpdateMin => Math.Min,
-                LazySegmentTreeMode.UpdateMax => Math.Max,
-                LazySegmentTreeMode.ModAddition => (l1, l2) => (l1 + l2) % ModInt.ModValue,
-                _ => (l1, l2) => (l1 + l2),
-            };
-            UpdateLazyToActualMethod = mode switch
-            {
-                LazySegmentTreeMode.Addition => (l1, l2, count) => l1 + l2 * count ,
-                LazySegmentTreeMode.ModAddition => (l1, l2, count) => (l1 + (l2 * count) % ModInt.ModValue) % ModInt.ModValue,
-                _ => (x, m, _) => m
-            };
-            SetLazyDataMethod = mode switch
-            {
-                LazySegmentTreeMode.Addition => (l1, l2) => (l1 + l2),
-                LazySegmentTreeMode.ModAddition => (l1, l2) => (l1 + l2) % ModInt.ModValue,
-                _ => (m1, m2) => m2
-            };
-        }
-
-        private static long GetInvalidValue(LazySegmentTreeMode mode) =>
-            mode switch
-            {
-                LazySegmentTreeMode.UpdateMin => Common.Infinity,
-                LazySegmentTreeMode.UpdateMax => -Common.Infinity,
-                _ => 0
-            };
-    }
-    
-    public class LazySegmentTreeExtend<T> where T : IEquatable<T>
+    /// <summary>
+    /// 遅延評価セグメント木
+    /// </summary>
+    /// <remarks>
+    /// https://atcoder.github.io/ac-library/production/document_ja/lazysegtree.html
+    /// https://qiita.com/katsumata_yusuke/items/2081690ae959c06b05a1
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    public class LazySegmentTree<T>
     {
         private T[] data;
         private T[] lazyData;
-        public Func<T, T, T> UpdateMethod { get; set; }
-        public Func<T, T, int, T> UpdateLazyToActualMethod { get; set; }
-        public Func<T, T, T> SetLazyDataMethod { get; set; }
-        public T InvalidValue { get; set; }
+        /// <summary> 全ノードの集計方法を表す二項演算 </summary>
+        /// <example> 最終的に和を得たいなら (a,b) = a + b のようにする </example>
+        public Func<(T A, T B), T> Operation { get; set; }
+        /// <summary> 自ノードの更新方法 f(x) 元の値を更新によりどのように保持しておくか  </summary>
+        /// <example> 更新が置換なら (cur,laz) => laz, 乗算なら (f,g) => cur * laz など </example>
+        public Func<(T Current, T Lazy), T> Mapping { get; set; }
+        /// <summary> 更新区間が重なったときの更新の合成方法 f∘g </summary>
+        /// <example> 更新が置換なら (f,g) => g, 乗算なら (f,g) => f * g など </example>
+        public Func<(T F, T G), T> Composition { get; set; }
+        /// <summary> 単位元 </summary>
+        public T E { get; set; }
         private int n;
         private int count;
+        private int height = 0;
 
-        public LazySegmentTreeExtend(int count, T invalidValue = default)
+        public LazySegmentTree(int count, T e = default)
         {
             this.count = count;
-            InvalidValue = invalidValue;
+            E = e;
 
             n = 1;
             while (n < count)
             {
-                n *= 2;
+                n <<= 1;
+                height++;
             }
 
-            data = Enumerable.Repeat(InvalidValue, 2 * n - 1).ToArray();
-            lazyData = Enumerable.Repeat(InvalidValue, 2 * n - 1).ToArray();
+            data = Enumerable.Repeat(E, 2 * n - 1).ToArray();
+            lazyData = Enumerable.Repeat(E, 2 * n - 1).ToArray();
         }
         
+        [MethodImpl(256)]
         public void Build(Func<int, T> update)
         {
             for (int i = 0; i < count; i++)
@@ -84,64 +57,89 @@ namespace Qlibrary
             }
             for (int i = n - 2; i >= 0; i--)
             {
-                data[i] = UpdateMethod(data[2 * i + 1], data[2 * i + 2]);
+                data[i] = Operation((data[2 * i + 1], data[2 * i + 2]));
             }
         }
 
+        [MethodImpl(256)]
         private void Evaluate(int k)
         {
-            if (lazyData[k].Equals(InvalidValue)) {return;}
+            if (lazyData[k].Equals(E)) {return;}
 
             if (k < n - 1)
             {
-                lazyData[2 * k + 1] = SetLazyDataMethod(lazyData[2 * k + 1], lazyData[k]);
-                lazyData[2 * k + 2] = SetLazyDataMethod(lazyData[2 * k + 2], lazyData[k]);
+                lazyData[2 * k + 1] = Composition((lazyData[2 * k + 1], lazyData[k]));
+                lazyData[2 * k + 2] = Composition((lazyData[2 * k + 2], lazyData[k]));
             }
 
-            int cc = k switch
-            {
-                0 => n,
-                _ => 1 << (MathPlus.Digit(n, 2) - MathPlus.Digit(k+1, 2))
-            };
-            data[k] = UpdateLazyToActualMethod(data[k], lazyData[k], cc);
-            lazyData[k] = InvalidValue;
+            // int cc = k switch
+            // {
+            //     0 => n,
+            //     _ => 1 << (MathPlus.Digit(n, 2) - MathPlus.Digit(k+1, 2))
+            // };
+            data[k] = Mapping((data[k], lazyData[k]));
+            lazyData[k] = E;
         }
 
+        [MethodImpl(256)]
         public void Update(int left, int right, T value)
         {
             Update(left, right + 1, value, 0, 0, n);
         }
 
+        [MethodImpl(256)]
         private void Update(int a, int b, T x, int k, int l, int r)
         {
             Evaluate(k);
             if (r <= a || b <= l) {return;}
             if (a <= l && r <= b)
             {
-                lazyData[k] = SetLazyDataMethod(lazyData[k], x);
+                lazyData[k] = Composition((lazyData[k], x));
                 Evaluate(k);
             }
             else
             {
                 Update(a, b, x, 2 * k + 1, l, (l + r) / 2);
                 Update(a, b, x, 2 * k + 2, (l + r) / 2, r);
-                data[k] = UpdateMethod(data[2 * k + 1], data[2 * k + 2]);
+                data[k] = Operation((data[2 * k + 1], data[2 * k + 2]));
             }
         }
 
+        [MethodImpl(256)]
         public T Query(int left, int right)
         {
             return Query(left, right + 1, 0, 0, n);
         }
 
+        [MethodImpl(256)]
         private T Query(int a, int b, int k, int l, int r)
         {
             Evaluate(k);
-            if (r <= a || b <= l) return InvalidValue;
+            if (r <= a || b <= l) return E;
             if (a <= l && r <= b) return data[k];
             T vl = Query(a, b, 2 * k + 1, l, (l + r) / 2);
             T vr = Query(a, b, 2 * k + 2, (l + r) / 2, r);
-            return UpdateMethod(vl, vr);
+            return Operation((vl, vr));
+        }
+
+        public override string ToString()
+        {
+            var v = 1 << height;
+            int index = 0;
+            var sb = new StringBuilder();
+            while (v > 0)
+            {
+                sb.AppendLine($"=== Depth {index} ===");
+                for (int i = 0; i < n / v; i++)
+                {
+                    sb.Append($"{v * i} to {v * i + v - 1} ... ");
+                    sb.AppendLine(Query(v * i, v * i + v - 1).ToString());
+                }
+                v >>= 1;
+                index++;
+            }
+            sb.AppendLine();
+            return sb.ToString();
         }
     }
 }
