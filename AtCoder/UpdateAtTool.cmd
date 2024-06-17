eeff7f10649a4f22d8f716cd3671147cda9a9212
Qlion <lovage.ew@gmail.com>
2024-06-18 00:34:56 +0900

    atcoder-toolsに合わせる

--- AtCoder/UpdateAtTool.cmd
+++ AtCoder/UpdateAtTool.cmd
@@ -2,14 +2,14 @@
 pip3 install atcoder-tools --upgrade
 
 rem そんなに使わないので一旦固定アドレスで・・・
-set input=C:\Python311\Lib\site-packages\atcodertools\common\language.py
-set output=C:\Python311\Lib\site-packages\atcodertools\common\language2.py
+set input=C:\Python312\Lib\site-packages\atcodertools\common\language.py
+set output=C:\Python312\Lib\site-packages\atcodertools\common\language2.py
 
 setlocal enabledelayedexpansion
 
 for /f "delims=" %%a in (%input%) do (
     set line=%%a
-    echo !line:".*C# \\(.NET*"=".*C# 11.0 AOT \\(.NET*"!>>%output%
+    echo !line:".*C# \\(Mono.*|.*C# 11.0 "=".*C# \\(Mono.*|.*C# 11.0 AOT"!>>%output%
 )
 
 copy %output% %input%
