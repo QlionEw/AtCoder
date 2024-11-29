19f49fa790c53e9ac554204502ec15984744fe54
Qlion <lovage.ew@gmail.com>
2024-11-30 01:21:58 +0900

    [ZobristHash] 軽微改善

--- Qlibrary/DataStructure/ZobristHash.cs
+++ Qlibrary/DataStructure/ZobristHash.cs
@@ -6,8 +6,9 @@ namespace Qlibrary
     public class ZobristHash : ZobristHash<long>{}
     public class ZobristHash<T>
     {
+        public bool IsSize64 { get; set; } = true;
         private readonly Dictionary<T, long> hashDict = new Dictionary<T, long>();
-        private readonly Random rand = new Random();
+        private readonly Random rand = new Random(DateTime.Now.Millisecond);
 
         public void Register(params T[] values)
         {
@@ -15,7 +16,11 @@ namespace Qlibrary
             {
                 if (hashDict.ContainsKey(value)) {continue;}
                 
-                long randomValue = ((long)rand.Next(0, int.MaxValue) << 32) + rand.Next(0, int.MaxValue);
+                long randomValue = rand.Next(0, int.MaxValue);
+                if (IsSize64)
+                {
+                    randomValue += ((long)rand.Next(0, int.MaxValue) << 32);
+                }
                 hashDict.Add(value, randomValue);
             }
         }
